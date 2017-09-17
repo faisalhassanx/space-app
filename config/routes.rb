cf9f6e8de127636015691e760ca03031559a54de
Faisal Hassan <faisal.hassanx@gmail.com>
2017-09-17 06:08:33 +0000

    Add contact page with TDD

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
-
+  get '/contact' => 'static_pages#contact'
+  get '/home' => 'static_pages#home'
   get '/about' => 'static_pages#about'
 
   root "static_pages#home"
