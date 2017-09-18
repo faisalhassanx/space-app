58d57399effbf36268e684a2c06e353769fb2773
Faisal Hassan <faisal.hassanx@gmail.com>
2017-09-18 22:36:59 +0000

    Add help page with TDD

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  get '/help' => 'static_pages#help'
   get '/contact' => 'static_pages#contact'
   get '/home' => 'static_pages#home'
   get '/about' => 'static_pages#about'
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -24,5 +24,11 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_select "title", "Contact | #{@base_title}"
   end
+  
+  test "should get help url" do
+    get help_url
+    assert_response :success
+    assert_select "title", "Help | #{@base_title}"
+  end
 
 end
