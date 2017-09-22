d9841335ce0bbad3dd2689c1d48c6292768b1cf2
Faisal Hassan <faisal.hassanx@gmail.com>
2017-09-22 23:00:11 +0000

    Remove get home route and add login to nav

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
 
   get '/help' => 'static_pages#help'
   get '/contact' => 'static_pages#contact'
-  get '/home' => 'static_pages#home'
   get '/about' => 'static_pages#about'
 
   root "static_pages#home"
