e8823ea0a4d83963d09ba7ab060fa1ce9dbae736
Faisal Hassan <faisal.hassanx@gmail.com>
2017-09-24 23:49:43 +0000

    Add tests for user model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ApplicationRecord
-  validates :name, presence: true
-  validates :email, presence: true, uniqueness: true
+  validates :name, presence: true, length: { in: 4..50 }
+  validates :email, presence: true, length: { in: 4..255 }
 end
