e509c94e81540f1fddbac301d22237d5d5cb38d5
Faisal Hassan <faisal.hassanx@gmail.com>
2017-09-26 22:48:29 +0000

    Add password digest to users table, user model test setup and clear users fixture

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,11 @@
 class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\-.]+\.[a-z]+\z/i
   
+  before_save { self.email = email.downcase }
   validates :name, presence: true,  length: { in: 4..50 }
   validates :email, presence: true, length: { in: 4..255 }, 
                                     uniqueness: { case_sensitive: false }, 
                                     format: { with: VALID_EMAIL_REGEX }
+                                    
+  has_secure_password
 end
