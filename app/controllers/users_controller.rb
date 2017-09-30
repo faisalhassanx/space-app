c8e3f2c5ec434bd4157e6c8ac0015ba149b059df
Faisal Hassan <faisal.hassanx@gmail.com>
2017-09-30 22:58:22 +0000

    Edit sign up form and whitelist user params in the users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
   
+  def index
+  end
+  
   def show
     @user = User.find_by(params[:id])
   end
@@ -7,5 +10,30 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+  
+  def edit
+  end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+  
+  def update
+  end
+  
+  def destroy
+  end
+  
+  private
+  
+    def user_params
+      params.require(:user).permit(:name, :email, :password, 
+                                                  :password_confirmation)
+    end
   
 end
