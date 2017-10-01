4f89ec81aa1fea1331774e27d2fa53a19f49c5cf
Faisal Hassan <faisal.hassanx@gmail.com>
2017-10-01 21:08:54 +0000

    Add flash messages for user signups

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,8 +17,10 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to root_path
+      flash[:success] = "Welcome to the Space App"
+      redirect_to @user
     else
+      flash[:danger] = "Please try again"
       render 'new'
     end
   end
