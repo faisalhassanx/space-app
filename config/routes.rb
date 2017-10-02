aaa0527b4e64020c35835f7e7c726f9b40d4d559
Faisal Hassan <faisal.hassanx@gmail.com>
2017-10-02 00:29:43 +0000

    Add routes for logout and edit login routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
 
-  get 'sessions/new'
+  get    '/login'  => 'sessions#new'
+  post   '/login'  => 'sessions#create'
+  delete '/logout' => 'sessions#destroy'
 
   resources :users
-  get '/signup' => 'users#new'
+  get '/signup'  => 'users#new'
 
-  get '/help' => 'static_pages#help'
+  get '/help'    => 'static_pages#help'
   get '/contact' => 'static_pages#contact'
-  get '/about' => 'static_pages#about'
+  get '/about'   => 'static_pages#about'
 
   root "static_pages#home"
   
